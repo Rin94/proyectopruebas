bdc5c0ba591c947ca1bd15ee38c5ca0ecc1432b9
Jared Raku <vagrant@vagrant.vagrant-debian-amd64>
2016-11-22 13:44:56 -0600

    ds store

--- facturador/trabajos/views.py
+++ facturador/trabajos/views.py
@@ -5,10 +5,14 @@ from trabajos.forms import trabajosForm, lista_articulosForm
 # Create your views here.
 def nuevo(request):
     if request.method=='POST':
+
         form=trabajosForm(request.POST)
+        form2=trabajosForm(request.POST)
+        print(request.POST)
+
         if form.is_valid():
-            form.save()
-            return redirect('#')
+
+            return redirect('base.html')
     else:
         form=trabajosForm()
         form2=lista_articulosForm()
